9e831d86e9e73377297bfb171515615e7ba307c5
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-02-17 17:27:07 -0800

    initialized the KnightPathFinder and required PolyTreeNode

--- practice-for-ch-ruby-knights-travails-long-practice/KnightPathFinder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice/KnightPathFinder.rb
@@ -1,9 +1,17 @@
+require_relative "../PolyTreeNode/practice-for-ch-ruby-poly-tree-node-long-practice/lib/tree_node.rb"
 class KnightPathFinder
 
 
-initialize()
+    def initialize(start_pos)
+        @start_pos = start_pos
+        @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions =
+        self.build_move_tree
+    end
 
-end
+    def self.valid_moves(pos)
+
+    end
 
 
 end
